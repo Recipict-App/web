2a73e5e729853e5df9e446e4ddf04dcd5b26d61a
draihan <draihan@student.ubc.ca>
2024-03-25 02:29:14 -0700

    Update Image attributes in page component

--- recipict-web/src/app/page.tsx
+++ recipict-web/src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Image src="/IMAGE.png" width={10000} height={3484} alt="recipict" />
+      <Image
+        src="/IMAGE.png"
+        draggable="false"
+        width={10000}
+        height={3484}
+        alt="recipict"
+      />
     </main>
   );
 }
